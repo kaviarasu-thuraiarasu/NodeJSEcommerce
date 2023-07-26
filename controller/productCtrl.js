cc66e8abd3f53bddbc1e15d53bae27e372dc1dc4
kaviarasu-thuraiarasu <kaviarasu.thuraiarasu@gmail.com>
2023-07-27 01:25:14 +0530

    Product Controller

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,21 +1,37 @@
 const productSchema = require("../models/productModel")
 
-const createProduct = async ()=>{
+const createProduct = async (req,res,next)=>{
+
+    const {title} = req.body
+    if(title) req.body.slug = title
+    const product = await productSchema.create(req.body)
+    res.json(product)
 
 }
 
-const getProduct = async ()=>{
+const getProduct = async (req,res,next)=>{
+    const {id} = req.params
+    const product = await productSchema.findById({_id:id})
+    res.json(product)
     
 }
-const getAllProduct = async ()=>{
-    
+const getAllProduct = async (req,res,next)=>{
+    const product = await productSchema.find({})
+    res.json(product)
 }
-const deleteProduct = async ()=>{
-    
+const deleteProduct = async (req,res,next)=>{
+    const {id} = req.params
+    await productSchema.findByIdAndDelete({_id:id})
+    res.send("Deleted Successfully")
 }
-const updateProduct = async ()=>{
-    
+const updateProduct = async (req,res,next)=>{
+
+    const {id} = req.params
+    await productSchema.findByIdAndUpdate({_id:id},req.body,{new:true})
+    res.send("updated Successfully")
 }
+    
+
 module.exports = {
     createProduct,
     getProduct,
